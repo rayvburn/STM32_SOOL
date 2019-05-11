e67b929fabe421d511c0da2a2e97c6a4a3a1a671
Jaroslaw Karwowski <j.karwowski@stud.elka.pw.edu.pl>
2019-05-12 00:31:13 +0200

    Updated USART_DMA example

--- src/Peripherals/USART/USART_DMA_example.c
+++ src/Peripherals/USART/USART_DMA_example.c
@@ -14,7 +14,9 @@ int main(void)
 
 	/* clock configuration needed */
 
-	volatile USART_DMA_Periph usart_debug = SOOL_USART_DMA_Init(USART2, 115200, 26);
+	volatile USART_DMA_Periph usart_debug = SOOL_USART_DMA_Init(USART2, 115200, 10); // 26);
+
+	/* Place handlers into proper global IRQHandlers */
 	DMA_IRQ_Handler_SetUsartDebug(&usart_debug);
 	USART_IRQ_Handler_SetUsartDebug(&usart_debug);
 	// -------------------------------------------
@@ -46,8 +48,44 @@ int main(void)
 		}
 	}
 
-	usart_debug.ActivateReading(&usart_debug);
-	usart_debug.Send(&usart_debug, "abcdefpo123qwdop45qwkdopdasd56ka123oadw1312ps\n\0");
+	/* Calling ActivateReading() is needed if you DO NOT want
+	 * the following characters to append current string -
+	 * it sets the buffer pointer back to the start
+	 * WITHOUT clearing its contents */
+	//	usart_debug.ActivateReading(&usart_debug);
+
+	/* Clearing does not protect the following received characters against appending
+	 * the current string - it just clears a whole buffer */
+	usart_debug.ClearRxBuffer(&usart_debug);
+
+	/* Clearing the TX buffer is not needed as long as you are sure
+	 * that previous data weren't longer (in terms of length) than
+	 * the following (unnecessary characters need to be trimmed) */
+	usart_debug.Send(&usart_debug, "123456789 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33\n");
+	for ( int i = 0; i < 500000; i++ ) {
+		if ( i == 40000 ) {
+
+		}
+	}
+
+	usart_debug.DeactivateReading(&usart_debug);
+	usart_debug.Send(&usart_debug, "poiuyewq");
+
+
+	uint8_t line_was_busy = 0;
+	if ( usart_debug.IsTxLineBusy(&usart_debug) ) {
+		line_was_busy = 1;
+		int abcd = 0;
+		abcd++;
+	}
+
+	for ( int i = 0; i < 500000; i++ ) {
+		if ( i == 40000 ) {
+
+		}
+	}
+
+	uint8_t data_rec = usart_debug.IsDataReceived(&usart_debug);
 
 	for ( int i = 0; i < 500000; i++ ) {
 		if ( i == 40000 ) {
@@ -56,5 +94,29 @@ int main(void)
 	}
 
 	const volatile Array_String* temp = usart_debug.GetRxData(&usart_debug);
+
+	int abc = 0;
+	abc++;
+	abc++;
+
+	data_rec = usart_debug.IsDataReceived(&usart_debug);
+	abc++;
+	abc++;
+
+	usart_debug.ClearRxBuffer(&usart_debug);
+	abc++;
+	abc++;
+
+	usart_debug.RestoreBuffersInitialSize(&usart_debug);
+	abc++;
+	abc++;
+
+	usart_debug.Destroy(&usart_debug);
+	abc++;
+	abc++;
+
+	while (1) { }
+
+	return (0);
 
 }
