ea86f5a2c9316d6c7b01b3a868f5519dd66e4b9d
Jaroslaw Karwowski <j.karwowski@stud.elka.pw.edu.pl>
2019-05-21 15:55:57 +0200

    WIP - Object-oriented FSM class

--- inc/sool/Workflow/FiniteStateMachine.h
+++ inc/sool/Workflow/FiniteStateMachine.h
@@ -8,27 +8,86 @@
 #ifndef FINITESTATEMACHINE_H_
 #define FINITESTATEMACHINE_H_
 
-#include <sool/Peripherals/TIM/SystickTimer.h> // DEPRECATED?
+#include <sool/Peripherals/TIM/SystickTimer.h>
 #include <stdint.h>
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+struct _SOOL_FSM_StateStruct {
+	uint8_t  current;
+	uint8_t  last;
+	uint32_t MIN_DURATION;
+	uint8_t  transition_flag;
+	uint32_t start_time;
+};
+
+typedef struct _SOOL_FSMStruct SOOL_FSM;
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+/*
+ * Below meant to be some kind of a vector (or C++'s map) storing state id along with state's transition function
+//
+//struct _SOOL_FSM_StateInfoStruct {
+//	uint8_t	state_id;
+//	uint8_t (*TransitionFunction)(SOOL_FSM *fsm, uint8_t arg_num, ...);
+//};
+// 	and corresponding functions
+//	void (*AddState)(SOOL_FSM *fsm, const uint8_t state_id, uint8_t (*TransitionFunction)(SOOL_FSM *fsm, uint8_t arg_num, ...));
+//	uint8_t (*ExecuteTransitionFunction)(SOOL_FSM *fsm);
+ */
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+/**
+ * Finite State Machine `alike` class which manages states switching in an application;
+ * uses a SysTick timer configured by default (1000 Hz)
+ */
+struct _SOOL_FSMStruct {
+
+	struct _SOOL_FSM_StateStruct	_state;
+	uint8_t (*SwitchToState)(SOOL_FSM *fsm, uint8_t state_id);
+	void 	(*SetMinStateDuration)(SOOL_FSM *fsm, uint32_t ms);		/* useful when states are switched via push-buttons */
+	uint8_t (*GetCurrentState)(SOOL_FSM *fsm);						/* to be used in main's while(1) to choose proper handler */
+	uint8_t (*GetStateTransitionFlag)(SOOL_FSM *fsm); 				/* useful for handling events after state switch */
+	uint8_t (*IsTerminalConditionFulfilled)(SOOL_FSM *fsm, uint8_t predicate);
+
+};
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+/**
+ * Creates a simple FSM instance which uses a SysTick timer configured by default (1000 Hz)
+ * @param init_state_id - ID of an initial state
+ * @param execute_on_entry - 0 prevents the initial state's on-entry action execution
+ * @param min_duration_ms - minimum duration of a state (in milliseconds)
+ * @return FSM object
+ */
+extern SOOL_FSM SOOL_Workflow_FSM_Init(uint8_t init_state_id, uint8_t execute_on_entry, uint32_t min_duration_ms);
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+
 
 
 //extern uint8_t SOOL_FSM_AddState(const uint8_t state_id, uint8_t (*TransitionFunction)(...));
 
-typedef enum {
-
-	IDLE_BLINKING = 0,
-	ARM_BULLET_ADJUSTMENTS,
-	ACCELERATING,
-	BULLET_ROLLING,
-	BULLET_STOPPED,
-
-} FSMStates;
-
-extern void 		SFM_SwitchToState(FSMStates);
-extern void			SFM_SetMinStateDuration(uint32_t tithings_of_sec);
-extern uint8_t 		SFM_GetStateTransitionFlag();
-extern FSMStates	SFM_GetLastState();
-extern FSMStates	SFM_GetCurrentState();
-extern uint8_t		SFM_IsTimingTerminalConditionFulfilled();
+//typedef enum {
+//
+//	IDLE_BLINKING = 0,
+//	ARM_BULLET_ADJUSTMENTS,
+//	ACCELERATING,
+//	BULLET_ROLLING,
+//	BULLET_STOPPED,
+//
+//} FSMStates;
+//
+//extern void 		SFM_SwitchToState(FSMStates);
+//extern void			SFM_SetMinStateDuration(uint32_t tithings_of_sec);
+//extern uint8_t 		SFM_GetStateTransitionFlag();
+//extern FSMStates	SFM_GetLastState();
+//extern FSMStates	SFM_GetCurrentState();
+//extern uint8_t		SFM_IsTimingTerminalConditionFulfilled();
 
 #endif /* FINITESTATEMACHINE_H_ */
--- src/sool/Workflow/FiniteStateMachine.c
+++ src/sool/Workflow/FiniteStateMachine.c
@@ -7,6 +7,124 @@
 
 #include <sool/Workflow/FiniteStateMachine.h>
 
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+static uint8_t FSM_SwitchToState(SOOL_FSM *fsm, uint8_t state_id);
+static void FSM_SetMinStateDuration(SOOL_FSM *fsm, uint32_t ms);		/* useful when states are switched via push-buttons */
+static uint8_t FSM_GetCurrentState(SOOL_FSM *fsm);						/* to be used in main's while(1) to choose proper handler */
+static uint8_t FSM_GetStateTransitionFlag(SOOL_FSM *fsm); 				/* useful for handling events after state switch */
+static uint8_t FSM_IsTimingTerminalConditionFulfilled(SOOL_FSM *fsm);
+static uint8_t FSM_IsTerminalConditionFulfilled(SOOL_FSM *fsm, uint8_t predicate);
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+SOOL_FSM SOOL_Workflow_FSM_Init(uint8_t init_state_id, uint8_t execute_on_entry, uint32_t min_duration_ms) {
+
+	/* Object to be returned */
+	SOOL_FSM fsm;
+
+	/* Set state structure fields */
+	fsm._state.start_time = 0;
+	fsm._state.MIN_DURATION = min_duration_ms;
+	fsm._state.current = init_state_id;
+	fsm._state.last = init_state_id;
+	(execute_on_entry > 0) ? (fsm._state.transition_flag = 1) : (fsm._state.transition_flag = 0);
+
+	/* Set methods pointers */
+	fsm.GetCurrentState = FSM_GetCurrentState;
+	fsm.GetStateTransitionFlag = FSM_GetStateTransitionFlag;
+	fsm.IsTerminalConditionFulfilled = FSM_IsTerminalConditionFulfilled;
+	fsm.SetMinStateDuration = FSM_SetMinStateDuration;
+	fsm.SwitchToState = FSM_SwitchToState;
+
+	return (fsm);
+
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+static uint8_t FSM_SwitchToState(SOOL_FSM *fsm, uint8_t state_id) {
+
+	/* Process only if state changed */
+	if ( fsm->_state.current != state_id ) {
+
+		/* Check if minimum duration condition is fulfilled */
+		if ( FSM_IsTimingTerminalConditionFulfilled(fsm) ) {
+
+			fsm->_state.start_time = SOOL_Periph_TIM_SysTick_GetMillis();
+			fsm->_state.last = fsm->_state.current;
+			fsm->_state.current = state_id;
+			fsm->_state.transition_flag = 1;
+
+			// indicate success
+			return (1);
+
+		}
+
+	}
+
+	// indicate fail
+	return (0);
+
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+static void FSM_SetMinStateDuration(SOOL_FSM *fsm, uint32_t ms) {
+	fsm->_state.MIN_DURATION = ms;
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+static uint8_t FSM_GetCurrentState(SOOL_FSM *fsm) {
+	return (fsm->_state.current);
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+/**
+ * @param fsm
+ * @return If transition flag is SET then it will be available only once, after each check it is cleared
+ */
+static uint8_t FSM_GetStateTransitionFlag(SOOL_FSM *fsm) {
+	uint8_t temp = fsm->_state.transition_flag;
+	fsm->_state.transition_flag = 0;
+	return (temp);
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+static uint8_t FSM_IsTimingTerminalConditionFulfilled(SOOL_FSM *fsm) {
+
+	/* If MIN_DURATION is 0 - return 1 immediately */
+	if ( fsm->_state.MIN_DURATION == 0 ) {
+		return (1);
+	}
+
+	/* If enough amount of time has passed - return 1 */
+	if ( (SOOL_Periph_TIM_SysTick_GetMillis() - fsm->_state.start_time) > fsm->_state.MIN_DURATION ) {
+		return (1);
+	}
+
+	return (0);
+
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+/**
+ * Checks whether state's terminal condition is fulfilled
+ * @param fsm
+ * @param predicate - logically-and`ed set of flags (like: cond1 & cond2 & cond3)
+ * @return 1 when terminal predicate is fulfilled
+ */
+static uint8_t FSM_IsTerminalConditionFulfilled(SOOL_FSM *fsm, uint8_t predicate) {
+	if ( predicate ) { return (1); } else { return (0); }
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+/*
 // initial conditions
 static uint8_t state_changed = 1;
 static FSMStates last_state = BULLET_STOPPED;
@@ -56,3 +174,4 @@ uint8_t SFM_IsTimingTerminalConditionFulfilled() {
 	return 0;
 
 }
+*/
