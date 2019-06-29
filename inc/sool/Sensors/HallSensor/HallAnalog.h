be7eb65f6d9dc955cdff701ab46d1b23fe24acf4
Jaroslaw Karwowski <j.karwowski@stud.elka.pw.edu.pl>
2019-06-29 02:37:42 +0200

    WIP - HallADC draft

---
+++ inc/sool/Sensors/HallSensor/HallAnalog.h
@@ -0,0 +1,16 @@
+/*
+ * HallAnalog.h
+ *
+ *  Created on: 24.06.2019
+ *      Author: user
+ */
+
+#ifndef INC_SOOL_SENSORS_HALLSENSOR_HALLANALOG_H_
+#define INC_SOOL_SENSORS_HALLSENSOR_HALLANALOG_H_
+
+#include "stm32f10x.h"
+// ADC
+
+// add this via addition of ADC_DMA channel ctor
+
+#endif /* INC_SOOL_SENSORS_HALLSENSOR_HALLANALOG_H_ */
---
+++ src/sool/Sensors/HallSensor/HallAnalog.c
@@ -0,0 +1,8 @@
+/*
+ * HallAnalog.c
+ *
+ *  Created on: 24.06.2019
+ *      Author: user
+ */
+
+#include "sool/Sensors/HallSensor/HallAnalog.h"
