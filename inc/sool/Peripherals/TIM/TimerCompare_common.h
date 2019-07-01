1a2903a1aae153af3811cd51c56e7dd4d5d31761
Jaroslaw Karwowski <j.karwowski@stud.elka.pw.edu.pl>
2019-07-01 22:01:03 +0200

    TIM - TimerCompare_common fixes, extensions

--- inc/sool/Peripherals/TIM/TimerCompare_common.h
+++ inc/sool/Peripherals/TIM/TimerCompare_common.h
@@ -10,8 +10,23 @@
 
 #include "stm32f10x_tim.h"
 
-extern uint8_t SOOL_Periph_TIM_IsCaptureCompareChannelEnabled(TIM_TypeDef* TIMx, uint16_t channel);
-extern uint16_t SOOL_Periph_TIM_GetCCR(TIM_TypeDef* TIMx, uint16_t channel);
-extern void SOOL_Periph_TIM_SetCCR(TIM_TypeDef* TIMx, uint16_t channel, uint16_t value);
+// DEPRECATED
+//typedef enum {
+//	SOOL_PERIPH_TIMCOMPARE_CCMR_CLEAR_ENABLE = 0,
+//	SOOL_PERIPH_TIMCOMPARE_CCMR_MODE,
+//	SOOL_PERIPH_TIMCOMPARE_CCMR_PRELOAD_ENABLE,
+//	SOOL_PERIPH_TIMCOMPARE_CCMR_FAST_ENABLE,
+//	SOOL_PERIPH_TIMCOMPARE_CCMR_SELECTION,
+//} SOOL_Periph_TIMCompare_BitCCMR;
+
+extern uint8_t SOOL_Periph_TIMCompare_IsCaptureCompareChannelEnabled(TIM_TypeDef* TIMx, uint16_t channel);
+extern uint16_t SOOL_Periph_TIMCompare_GetCCR(TIM_TypeDef* TIMx, uint16_t channel);
+extern void SOOL_Periph_TIMCompare_SetCCR(TIM_TypeDef* TIMx, uint16_t channel, uint16_t value);
+
+// DEPRECATED
+//extern void SOOL_Periph_TIMCompare_SetCCMR(TIM_TypeDef* TIMx, uint16_t channel, uint16_t mask);
+
+extern void SOOL_Periph_TIMCompare_EnableChannel(TIM_TypeDef* TIMx, uint16_t channel);
+extern void SOOL_Periph_TIMCompare_DisableChannel(TIM_TypeDef* TIMx, uint16_t channel);
 
 #endif /* INC_SOOL_PERIPHERALS_TIM_TIMERCOMPARE_COMMON_H_ */
--- src/sool/Peripherals/TIM/TimerCompare_common.c
+++ src/sool/Peripherals/TIM/TimerCompare_common.c
@@ -7,7 +7,7 @@
 
 #include "sool/Peripherals/TIM/TimerCompare_common.h"
 
-uint8_t SOOL_Periph_TIM_IsCaptureCompareChannelEnabled(TIM_TypeDef* TIMx, uint16_t channel) {
+uint8_t SOOL_Periph_TIMCompare_IsCaptureCompareChannelEnabled(TIM_TypeDef* TIMx, uint16_t channel) {
 
 	switch (channel) {
 
@@ -29,7 +29,7 @@ uint8_t SOOL_Periph_TIM_IsCaptureCompareChannelEnabled(TIM_TypeDef* TIMx, uint16
 
 }
 
-uint16_t SOOL_Periph_TIM_GetCCR(TIM_TypeDef* TIMx, uint16_t channel) {
+uint16_t SOOL_Periph_TIMCompare_GetCCR(TIM_TypeDef* TIMx, uint16_t channel) {
 
 	switch (channel) {
 
@@ -51,21 +51,21 @@ uint16_t SOOL_Periph_TIM_GetCCR(TIM_TypeDef* TIMx, uint16_t channel) {
 
 }
 
-void SOOL_Periph_TIM_SetCCR(TIM_TypeDef* TIMx, uint16_t channel, uint16_t value) {
+void SOOL_Periph_TIMCompare_SetCCR(TIM_TypeDef* TIMx, uint16_t channel, uint16_t value) {
 
 	switch (channel) {
 
 	case(TIM_Channel_1):
-		TIMx->CCR1 = value;
+		TIMx->CCR1 = (uint16_t)value;
 		break;
 	case(TIM_Channel_2):
-		TIMx->CCR2 = value;
+		TIMx->CCR2 = (uint16_t)value;
 		break;
 	case(TIM_Channel_3):
-		TIMx->CCR3 = value;
+		TIMx->CCR3 = (uint16_t)value;
 		break;
 	case(TIM_Channel_4):
-		TIMx->CCR4 = value;
+		TIMx->CCR4 = (uint16_t)value;
 		break;
 	default:
 		break;
@@ -73,3 +73,80 @@ void SOOL_Periph_TIM_SetCCR(TIM_TypeDef* TIMx, uint16_t channel, uint16_t value)
 
 }
 
+// DEPRECATED
+//void SOOL_Periph_TIMCompare_SetCCMR(TIM_TypeDef* TIMx, uint16_t channel, uint16_t mask) {
+//
+//	// There are 2 CCMRx registers available
+//	switch (channel) {
+//
+//	case(TIM_Channel_1):
+//		TIMx->CCMR1 = (uint16_t)mask;
+//		break;
+//
+//	case(TIM_Channel_2):
+//		TIMx->CCMR1 = (uint16_t)mask;
+//
+//		break;
+//	case(TIM_Channel_3):
+//		TIMx->CCMR2 = (uint16_t)mask;
+//		break;
+//
+//	case(TIM_Channel_4):
+//		TIMx->CCMR2 = (uint16_t)mask;
+//		break;
+//
+//	default:
+//		break;
+//
+//	}
+//
+//}
+
+void SOOL_Periph_TIMCompare_EnableChannel(TIM_TypeDef* TIMx, uint16_t channel) {
+
+	/* Enable the Channel x: Set the CCxE Bit */
+	switch (channel) {
+
+	case(TIM_Channel_1):
+		TIMx->CCER |= (uint16_t)TIM_CCER_CC1E;
+		break;
+
+	case(TIM_Channel_2):
+		TIMx->CCER |= (uint16_t)TIM_CCER_CC2E;
+		break;
+
+	case(TIM_Channel_3):
+		TIMx->CCER |= (uint16_t)TIM_CCER_CC3E;
+		break;
+
+	case(TIM_Channel_4):
+		TIMx->CCER |= (uint16_t)TIM_CCER_CC4E;
+		break;
+
+	}
+
+}
+void SOOL_Periph_TIMCompare_DisableChannel(TIM_TypeDef* TIMx, uint16_t channel) {
+
+	/* Disable the Channel x: Reset the CCxE Bit */
+	switch (channel) {
+
+	case(TIM_Channel_1):
+		TIMx->CCER &= (uint16_t)(~((uint16_t)TIM_CCER_CC1E));
+		break;
+
+	case(TIM_Channel_2):
+		TIMx->CCER &= (uint16_t)(~((uint16_t)TIM_CCER_CC2E));
+		break;
+
+	case(TIM_Channel_3):
+		TIMx->CCER &= (uint16_t)(~((uint16_t)TIM_CCER_CC3E));
+		break;
+
+	case(TIM_Channel_4):
+		TIMx->CCER &= (uint16_t)(~((uint16_t)TIM_CCER_CC4E));
+		break;
+
+	}
+
+}
