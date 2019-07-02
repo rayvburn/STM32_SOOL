566998d2da07d88c68eefc4261dd8ce75a4ae783
Jaroslaw Karwowski <j.karwowski@stud.elka.pw.edu.pl>
2019-07-02 16:12:53 +0200

    GPIO - GPIO_common - added functions based on StdPeriph, deleted param check

--- src/sool/Peripherals/GPIO/GPIO_common.c
+++ src/sool/Peripherals/GPIO/GPIO_common.c
@@ -7,3 +7,19 @@
 
 #include "sool/Peripherals/GPIO/GPIO_common.h"
 
+
+uint8_t SOOL_Periph_GPIO_ReadInputDataBit(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin) {
+	  return ( (uint8_t)(GPIOx->IDR & (uint32_t)GPIO_Pin) );
+}
+
+uint8_t SOOL_Periph_GPIO_ReadOutputDataBit(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin) {
+	return ( (uint8_t)(GPIOx->ODR & (uint32_t)GPIO_Pin) );
+}
+
+void SOOL_Periph_GPIO_SetBits(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin) {
+	GPIOx->BSRR = GPIO_Pin;
+}
+
+void GPIO_ResetBits(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin) {
+	GPIOx->BRR = GPIO_Pin;
+}
