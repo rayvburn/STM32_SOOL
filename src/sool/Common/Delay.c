014321bc171c07e168c61fb795ab41248a2241da
Jaroslaw Karwowski <j.karwowski@stud.elka.pw.edu.pl>
2019-08-22 10:59:30 +0200

    Delay - fixed stop value calculations, more accurate counting

--- src/sool/Common/Delay.c
+++ src/sool/Common/Delay.c
@@ -8,11 +8,13 @@
 #include "sool/Common/Delay.h"
 
 void SOOL_Common_Delay(long unsigned int ms, long unsigned int core_clock) {
-	long unsigned int stop = ms * core_clock / 72 * 4000; 	// experimentally
+	unsigned int core_clock_mhz = core_clock / 1000000;
+	long unsigned int stop = ms * (core_clock_mhz * 5000 / 72); 	// experimentally
 	for ( long unsigned int i = 0; i < stop; i++) { }
 }
 
 void SOOL_Common_DelayUs(long unsigned int us, long unsigned int core_clock) {
-	long unsigned int stop = us * core_clock / 72 * 4; 		// experimentally
+	unsigned int core_clock_mhz = core_clock / 1000000;
+	long unsigned int stop = us * (core_clock_mhz * 5 / 72); 	// experimentally
 	for ( long unsigned int i = 0; i < stop; i++) { }
 }
