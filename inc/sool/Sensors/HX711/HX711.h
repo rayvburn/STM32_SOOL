d3c50d05442bee28975db0996150bcc5c82c9cb9
Jaroslaw Karwowski <j.karwowski@stud.elka.pw.edu.pl>
2019-11-15 16:12:59 +0100

    WIP - Sensors/HX711 - interrupt-driven approach (not very implementation-friendly)

--- inc/sool/Sensors/HX711/HX711.h
+++ inc/sool/Sensors/HX711/HX711.h
@@ -9,7 +9,8 @@
 #define INC_SOOL_SENSORS_HX711_HX711_H_
 
 #include <sool/Peripherals/GPIO/PinConfig_Int.h>
-#include <sool/Peripherals/GPIO/PinConfig_NoInt.h>
+#include <sool/Peripherals/GPIO/PinConfig_AltFunction.h>
+#include <sool/Peripherals/TIM/TimerOutputCompare.h>
 #include <stdint.h>
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -24,7 +25,7 @@ struct _SOOL_HX711StateStruct {
 	int32_t				inc_per_unit; 			// either grams or kilos, it's up to an user
 	int32_t				data_last;				// ability to read the lastly measured value while already started a new measurement
 	int32_t				data_temp;
-	uint8_t				data_bits_left;			// how many interrupt calls to expect
+	int8_t				data_bits_left;			// how many interrupt calls to expect (signed to avoid overflow)
 
 };
 
@@ -36,46 +37,34 @@ struct _SOOL_HX711Struct {
 
 	// ----------- base class section
 	SOOL_PinConfig_Int 				base_dout;
-	SOOL_PinConfig_NoInt			base_sck;
+	SOOL_TimerOutputCompare			base_tim_sck;
 
 	// ----------- derived class section
 	struct _SOOL_HX711StateStruct 	_state;
 
-	/// @brief Measures and updates internal offset value. This is a blocking function, will hold
-	/// the whole application until finishes the procedure.
-	/// @param hx_ptr
-	/// @param samples_num
-	/// @return
-	uint8_t		(*CalculateOffset)(volatile SOOL_HX711 *hx_ptr, uint8_t samples_num);
+	uint8_t		(*IsDataReady)(volatile SOOL_HX711 *hx_ptr);
+	int32_t		(*GetData)(volatile SOOL_HX711 *hx_ptr);
 
-	uint8_t 	(*IsOffsetCalculated)(volatile SOOL_HX711 *hx_ptr);
+	// DEPRECATED: BLOCKING IMPLEMENTATION - blocking for about 30 us (safer because blocks interrupts, see power-down mode)
 
-	/// @brief Must forego the potential call of @ref StartMeasurement
-	/// @param hx_ptr
-	/// @return
-	uint8_t		(*IsDeviceReady)(volatile SOOL_HX711 *hx_ptr);
-	uint8_t		(*StartMeasurement)(volatile SOOL_HX711 *hx_ptr);
-	uint8_t		(*IsMeasurementReady)(volatile SOOL_HX711 *hx_ptr);
-	int32_t		(*GetLastMeasurement)(volatile SOOL_HX711 *hx_ptr);
-
-	uint8_t 	(*_InterruptHandler)(volatile SOOL_HX711 *hx_ptr); // a routine fired in a proper ISR (firstly it must check if interrupt has been triggered on sensor's EXTI line)
+	uint8_t		(*_TimerInterruptHandler)(volatile SOOL_HX711 *hx_ptr);
+	uint8_t 	(*_ExtiInterruptHandler)(volatile SOOL_HX711 *hx_ptr); // a routine fired in a proper ISR (firstly it must check if interrupt has been triggered on sensor's EXTI line)
 
 };
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-/// @brief
-/// @param dout_port
-/// @param dout_pin
-/// @param sck_port
-/// @param sck_pin
-/// @param gain
-/// @param offset: offset is optional, call calculate offset after that fcn if needed
-/// @param increment_per_unit
-/// @return
-/// @note Remember to enable NVIC of the interrupt-driven DOUT pin (EXTI is enabled during `StartMeasurement` routine
-extern volatile SOOL_HX711 SOOL_Sensor_HX711_Init(GPIO_TypeDef* dout_port, uint16_t dout_pin, GPIO_TypeDef* sck_port,
-												  uint16_t sck_pin, uint8_t gain, int32_t offset, int32_t increment_per_unit);
+/// @note This implementation requires separate timer for the sensor
+/// @note Timer TIMx interrupt (update event) is enabled and must be handled in the application (IRQHandlers setup)
+/// @note SCK pin must be wired according to the Timer channel
+/// @note Timer must be configured in such a way providing at most 500 kHz frequency (may be lower,
+/// but should be higher than 20 kHz due to entrance into power-down mode)
+/// @note SCK port and pin could've been automated but better stick to explicit arguments
+/// so the user knows what he does
+extern volatile SOOL_HX711 SOOL_Sensor_HX711_Init(GPIO_TypeDef* dout_port, uint16_t dout_pin,
+												  GPIO_TypeDef* sck_port, uint16_t sck_pin,
+												  TIM_TypeDef* TIMx, uint16_t channel,
+												  uint8_t gain, int32_t offset, int32_t increment_per_unit);
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
--- src/sool/Sensors/HX711/HX711.c
+++ src/sool/Sensors/HX711/HX711.c
@@ -8,40 +8,67 @@
 #include "sool/Sensors/HX711/HX711.h"
 #include "sool/Common/Delay.h"
 #include "sool/Memory/Array/ArrayInt32.h"
+#include "sool/Peripherals/GPIO/GPIO_common.h" // SOOL_Periph_GPIO_SetBits
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-static uint8_t	SOOL_HX711_CalculateOffset(volatile SOOL_HX711 *hx_ptr, uint8_t samples_num);
-static uint8_t 	SOOL_HX711_IsOffsetCalculated(volatile SOOL_HX711 *hx_ptr);
+static uint8_t	SOOL_HX711_TimerInterruptHandler(volatile SOOL_HX711 *hx_ptr);
+static uint8_t 	SOOL_HX711_ExtiInterruptHandler(volatile SOOL_HX711 *hx_ptr);
 
-static uint8_t 	SOOL_HX711_IsDeviceReady(volatile SOOL_HX711 *hx_ptr);
-static uint8_t	SOOL_HX711_StartMeasurement(volatile SOOL_HX711 *hx_ptr);
-static uint8_t	SOOL_HX711_IsMeasurementReady(volatile SOOL_HX711 *hx_ptr);
-static int32_t	SOOL_HX711_GetLastMeasurement(volatile SOOL_HX711 *hx_ptr);
+static uint8_t	SOOL_HX711_IsDataReady(volatile SOOL_HX711 *hx_ptr);
+static uint8_t 	SOOL_HX711_GetData(volatile SOOL_HX711 *hx_ptr);
 
-static uint8_t 	SOOL_HX711_InterruptHandler(volatile SOOL_HX711 *hx_ptr);
+// helper
+static uint8_t SOOL_HX711_ReadBit(volatile SOOL_HX711 *hx_ptr) ;
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-// helpers
-/// \brief Shifts the bit and returns a new value (0 or 1)
-/// \param hx_ptr: load cell instance
-/// \return
-static uint8_t 	SOOL_HX711_ShiftDataBit(volatile SOOL_HX711 *hx_ptr);
+// temp
+static void SOOL_HX711_SetHelperPinTIM(FunctionalState state);
+static void SOOL_HX711_SetHelperPinEXTI(FunctionalState state);
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-volatile SOOL_HX711 SOOL_Sensor_HX711_Init(GPIO_TypeDef* dout_port, uint16_t dout_pin, GPIO_TypeDef* sck_port,
-				  uint16_t sck_pin, uint8_t gain, int32_t offset, int32_t increment_per_unit) {
+volatile SOOL_HX711 SOOL_Sensor_HX711_Init(GPIO_TypeDef* dout_port, uint16_t dout_pin,
+										   GPIO_TypeDef* sck_port, uint16_t sck_pin,
+										   TIM_TypeDef* TIMx, uint16_t channel,
+										   uint8_t gain, int32_t offset, int32_t increment_per_unit)
+{
 
+	/* HX711-based load cell instance */
 	volatile SOOL_HX711 load_cell;
 
-	//
-	load_cell.base_dout = SOOL_Periph_GPIO_PinConfig_Initialize_Int(dout_port, dout_pin, GPIO_Mode_IN_FLOATING, EXTI_Trigger_Rising_Falling);
-	load_cell.base_sck  = SOOL_Periph_GPIO_PinConfig_Initialize_NoInt(sck_port, sck_pin, GPIO_Mode_Out_PP);
+	/* DT / DOUT pin configuration */										          // GPIO_Mode_IN_FLOATING
+	load_cell.base_dout = SOOL_Periph_GPIO_PinConfig_Initialize_Int(dout_port, dout_pin, GPIO_Mode_IPU, EXTI_Trigger_Rising_Falling);
 
-	// PD_SCK high (blocks data reception)
-	SOOL_Periph_GPIO_SetBits(sck_port, sck_pin);
+	/* Create an Alternative Functinon pin configuration;
+	 * at the moment this does not need to be stored internally,
+	 * pure configuration is enough. */
+	SOOL_PinConfig_AltFunction sck = SOOL_Periph_GPIO_PinConfig_Initialize_AltFunction(sck_port, sck_pin, GPIO_Mode_AF_PP);
+
+	////// helper
+	///
+	SOOL_Periph_GPIO_PinConfig_Initialize_NoInt(GPIOA, GPIO_Pin_9, GPIO_Mode_Out_PP);  // EXTI
+	SOOL_Periph_GPIO_PinConfig_Initialize_NoInt(GPIOA, GPIO_Pin_12, GPIO_Mode_Out_PP); // TIM
+	///
+	///
+
+	/* Create an instance of SOOL_TimerBasic */
+	// set prescaler to count microseconds
+//	uint16_t prescaler_us = (uint16_t)(SystemCoreClock / 2000000ul);
+	uint16_t prescaler_us = (uint16_t)(SystemCoreClock / 1000000ul);
+//	uint16_t period = 20;
+	uint16_t period = 20; // 2 us
+	volatile SOOL_TimerBasic tim_basic = SOOL_Periph_TIM_TimerBasic_Init(TIMx, prescaler_us, period, ENABLE);
+
+	/* Create an instance of SOOL_TimerOutputCompare */
+//	load_cell.base_tim_sck = SOOL_Periph_TIM_TimerOutputCompare_Init(tim_basic, channel, TIM_OCMode_PWM2, 1,
+//								DISABLE, TIM_OCIdleState_Set, TIM_OCPolarity_Low, TIM_OutputState_Enable);
+	load_cell.base_tim_sck = SOOL_Periph_TIM_TimerOutputCompare_Init(tim_basic, channel, TIM_OCMode_PWM2, // TIM_OCMode_Toggle,
+								//10, // NOTE: pulse must be in the range: [0 < PULSE < PERIOD]
+								10,
+								DISABLE, TIM_OCIdleState_Reset, TIM_OCPolarity_Low, TIM_OutputState_Enable);
+
+//	// PD_SCK high (blocks data reception)
+//	SOOL_Periph_GPIO_SetBits(sck_port, sck_pin);
 
 	// very very unlikely that the sensor offset will be 0 for any application
 	if ( offset != 0 ) {
@@ -70,21 +97,18 @@ volatile SOOL_HX711 SOOL_Sensor_HX711_Init(GPIO_TypeDef* dout_port, uint16_t dou
 	// scale
 	load_cell._state.inc_per_unit = increment_per_unit;
 
-	// Pull PD_SCK low - necessary for data retrieval
-	SOOL_Periph_GPIO_ResetBits(sck_port, sck_pin);
+//	// Pull PD_SCK low - necessary for data retrieval
+//	SOOL_Periph_GPIO_ResetBits(sck_port, sck_pin);
 
 	// methods
-	load_cell.CalculateOffset = SOOL_HX711_CalculateOffset;
-	load_cell.GetLastMeasurement = SOOL_HX711_GetLastMeasurement;
-	load_cell.IsDeviceReady = SOOL_HX711_IsDeviceReady;
-	load_cell.IsMeasurementReady = SOOL_HX711_IsMeasurementReady;
-	load_cell.IsOffsetCalculated = SOOL_HX711_IsOffsetCalculated;
-	load_cell.StartMeasurement = SOOL_HX711_StartMeasurement;
+	load_cell.GetData = SOOL_HX711_GetData;
+	load_cell.IsDataReady = SOOL_HX711_IsDataReady;
 
-	load_cell._InterruptHandler = SOOL_HX711_InterruptHandler;
+	load_cell._ExtiInterruptHandler  = SOOL_HX711_ExtiInterruptHandler;
+	load_cell._TimerInterruptHandler = SOOL_HX711_TimerInterruptHandler;
 
 	// state
-	load_cell._state.data_bits_left = 1;
+	load_cell._state.data_bits_left = -1;
 	load_cell._state.data_last = 0;
 	load_cell._state.data_temp = 0;
 	load_cell._state.flag_data_ready = 0;
@@ -94,180 +118,126 @@ volatile SOOL_HX711 SOOL_Sensor_HX711_Init(GPIO_TypeDef* dout_port, uint16_t dou
 
 }
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-static uint8_t SOOL_HX711_CalculateOffset(volatile SOOL_HX711 *hx_ptr, uint8_t samples_num) {
-
-	// data storage
-	SOOL_Array_Int32 data = SOOL_Memory_Array_Int32_Init(samples_num);
-
-	// wait until ready
-	while ( !hx_ptr->IsDeviceReady(hx_ptr) );
-
-	// collect as many samples as required
-	for ( uint8_t i = 0; i < samples_num; i++ ) {
-
-		while (!hx_ptr->StartMeasurement(hx_ptr));
-		while (!hx_ptr->IsMeasurementReady(hx_ptr));
-		data.Add(&data, hx_ptr->GetLastMeasurement(hx_ptr));
-
-	}
-
-	// calculate average
-	int64_t avg = 0;
-	for ( uint8_t i = 0; i < samples_num; i++ ) {
-		avg += data._data[i];
-	}
-	avg /= samples_num;
-
-	// update offset
-	hx_ptr->_state.offset = (int32_t)avg;
-
-	return (1);
-
-}
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-static uint8_t SOOL_HX711_IsOffsetCalculated(volatile SOOL_HX711 *hx_ptr) {
-	return (hx_ptr->_state.flag_offset_calculated);
-}
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-static uint8_t SOOL_HX711_IsDeviceReady(volatile SOOL_HX711 *hx_ptr) {
-
-	/* When  output  data  is  not  ready  for  retrieval,  digital  output  pin  DOUT  is  high.
-	 * Serial  clock  input PD_SCK should be low. */
-
-	// set PD_SCK to low
-	SOOL_Periph_GPIO_ResetBits(hx_ptr->base_sck.gpio.port, hx_ptr->base_sck.gpio.pin);
-
-	if ( SOOL_Periph_GPIO_ReadInputDataBit(hx_ptr->base_dout._gpio.port, hx_ptr->base_dout._gpio.pin) ) {
-		return (0); // not ready (because DOUT is high)
-	}
-
-	return (1); 	// ready (DOUT is low)
-
-}
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-static uint8_t SOOL_HX711_StartMeasurement(volatile SOOL_HX711 *hx_ptr) {
-
-	if ( hx_ptr->_state.flag_read_started ) {
-		return (0); // let the previous reading finish
-	}
-
-	/* Prepare an indicator for InterruptHandler */
-	hx_ptr->_state.flag_read_started = 0;
-
-	/* Enable EXTI on the DOUT line */
-	hx_ptr->base_dout.EnableEXTI(&hx_ptr->base_dout);
-
-    /* When output data is not ready for retrieval, digital output pin DOUT is high
-     * Serial clock input PD_SCK should be low. */
-	SOOL_Periph_GPIO_ResetBits(hx_ptr->base_sck.gpio.port, hx_ptr->base_sck.gpio.pin);
-
-	/* Reset internal state (except the `flag_read_started`) */
-	hx_ptr->_state.data_bits_left = 1; // 1 to avoid overflow (just in case), see the ISR
-	hx_ptr->_state.flag_data_ready = 0;
-	hx_ptr->_state.data_temp = 0;
-
-	return (1);
-
-}
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-static uint8_t SOOL_HX711_IsMeasurementReady(volatile SOOL_HX711 *hx_ptr) {
+static uint8_t SOOL_HX711_IsDataReady(volatile SOOL_HX711 *hx_ptr) {
 	uint8_t temp = hx_ptr->_state.flag_data_ready;
 	hx_ptr->_state.flag_data_ready = 0;
-	return (temp);
+	return (hx_ptr->_state.flag_data_ready);
 }
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-static int32_t SOOL_HX711_GetLastMeasurement(volatile SOOL_HX711 *hx_ptr) {
+static uint8_t SOOL_HX711_GetData(volatile SOOL_HX711 *hx_ptr) {
 	return (hx_ptr->_state.data_last);
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-static uint8_t SOOL_HX711_InterruptHandler(volatile SOOL_HX711 *hx_ptr) {
+static uint8_t SOOL_HX711_TimerInterruptHandler(volatile SOOL_HX711 *hx_ptr) {
 
-	if ( hx_ptr->_state.flag_read_started == 0 ) {
+	if ( !hx_ptr->_state.flag_read_started ) {
+		return (0);
+	}
 
-		// when DOUT goes to low, it indicates data is ready for retrieval.
-		if ( SOOL_Periph_GPIO_ReadInputDataBit(hx_ptr->base_dout._gpio.port, hx_ptr->base_dout._gpio.pin) != 0 ) {
-			return (0); // device not ready now
-		}
+	SOOL_HX711_SetHelperPinTIM(ENABLE);
+	/* To prevent unnecessary pulse generation */
+	uint8_t finished = 0;
+	if ( --hx_ptr->_state.data_bits_left == 0 ) {
+		hx_ptr->base_tim_sck.base.DisableNVIC(&hx_ptr->base_tim_sck.base);
+		hx_ptr->base_tim_sck.DisableChannel(&hx_ptr->base_tim_sck);
+		hx_ptr->base_tim_sck.base.EnableNVIC(&hx_ptr->base_tim_sck.base);
+		finished = 1;
+	}
 
-		hx_ptr->_state.flag_read_started = 1;
-		hx_ptr->_state.data_bits_left = 24 + 1; // 24 is a sensor-specific value, +1 to avoid overflow after subtraction, see the stop condition below
+	/* Read */
+	hx_ptr->_state.data_temp |= SOOL_HX711_ReadBit(hx_ptr);
+	hx_ptr->_state.data_temp <<= 1;
 
-	} else {
+	/* Process whole `word` */
+	if ( finished ) {
 
-		// check if reading is in progress
-		if ( --hx_ptr->_state.data_bits_left > 0 ) {
+		hx_ptr->_state.data_temp ^= 0x800000;
 
-			// shift a new data bit to the buffer
-			SOOL_HX711_ShiftDataBit(hx_ptr);
+//		hx_ptr->base_tim_sck.Stop(&hx_ptr->base_tim_sck);
 
-		} else {
+		// convert raw value to appropriate units
+		hx_ptr->_state.data_last = (hx_ptr->_state.data_temp + hx_ptr->_state.offset) / hx_ptr->_state.inc_per_unit;
 
-			// The 25th pulse at PD_SCK input will pull DOUT pin back to high.
-			// PD_SCK clock pulses should not be less than 25 or more than 27
-			// within one conversion period, to avoid causing serial communication error.
-			for ( uint8_t i = 0; i < hx_ptr->_state.gain; i++ ) {
-				SOOL_Periph_GPIO_SetBits(hx_ptr->base_sck.gpio.port, hx_ptr->base_sck.gpio.pin);
-				// TODO: some small delay MAY need to be applied
-				SOOL_Periph_GPIO_ResetBits(hx_ptr->base_sck.gpio.port, hx_ptr->base_sck.gpio.pin);
-			}
-
-			// finished reading, disable EXTI on the DOUT line
-			hx_ptr->base_dout.DisableEXTI(&hx_ptr->base_dout);
-
-			//
-			hx_ptr->_state.data_temp ^= 0x800000;
-
-			// convert raw value to appropriate units
-			hx_ptr->_state.data_last = (hx_ptr->_state.data_temp + hx_ptr->_state.offset) / hx_ptr->_state.inc_per_unit;
-
-			// update state
-			hx_ptr->_state.flag_data_ready = 1;
-			hx_ptr->_state.flag_read_started = 0;
-
-		}
+		// update state
+		hx_ptr->_state.flag_data_ready = 1;
+		hx_ptr->_state.flag_read_started = 0;
+		SOOL_HX711_SetHelperPinEXTI(DISABLE);
+		SOOL_HX711_SetHelperPinTIM(DISABLE);
+		return (1);
 
 	}
 
-	return (1);
+	SOOL_HX711_SetHelperPinTIM(DISABLE);
+	return (2);
 
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-// helper
-static uint8_t SOOL_HX711_ShiftDataBit(volatile SOOL_HX711 *hx_ptr) { // leaves SCK low
+static uint8_t SOOL_HX711_ExtiInterruptHandler(volatile SOOL_HX711 *hx_ptr) {
 
-	/* Each PD_SCK pulse shifts out one bit, starting with the MSB bit first,
-	 * until all 24 bits are shifted  out. */
-	SOOL_Periph_GPIO_SetBits(hx_ptr->base_sck.gpio.port, hx_ptr->base_sck.gpio.pin);
-	hx_ptr->_state.data_temp <<= 1; // shift left
-	// TODO: some small delay MAY need to be applied
-	SOOL_Periph_GPIO_ResetBits(hx_ptr->base_sck.gpio.port, hx_ptr->base_sck.gpio.pin);
+	/* DOUT falling edge detection */
+	if ( !SOOL_HX711_ReadBit(hx_ptr) && !hx_ptr->_state.flag_read_started ) {
 
-	// TODO: some small delay MAY need to be applied
-	uint8_t new_bit = SOOL_Periph_GPIO_ReadInputDataBit(hx_ptr->base_dout._gpio.port, hx_ptr->base_dout._gpio.pin);
-	hx_ptr->_state.data_temp |= new_bit;
+		// reset internal state
+		hx_ptr->_state.flag_read_started = 1;
+		hx_ptr->_state.data_bits_left = 24 + hx_ptr->_state.gain - 1;
 
-	return (new_bit);
+		// stop the timer to reset counter
+		hx_ptr->base_tim_sck.Stop(&hx_ptr->base_tim_sck);
+
+		// reset timer
+		hx_ptr->base_tim_sck.base.SetCounter(&hx_ptr->base_tim_sck.base, 0);
+
+		SOOL_HX711_SetHelperPinEXTI(ENABLE);
+
+		// enable OutputCompare channel of the timer
+		hx_ptr->base_tim_sck.EnableChannel(&hx_ptr->base_tim_sck);
+
+		// start the counter
+		hx_ptr->base_tim_sck.Start(&hx_ptr->base_tim_sck);
+
+		// reset buffer
+		hx_ptr->_state.data_temp = 0;
+
+		return (1);
+
+	}
+
+	return (0);
 
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-/*
- * //SOOL_Common_DelayUs(1, SystemCoreClock); // incorporation of a timer for such task is an overkill
- */
+static uint8_t SOOL_HX711_ReadBit(volatile SOOL_HX711 *hx_ptr) {
+	return (SOOL_Periph_GPIO_ReadInputDataBit(hx_ptr->base_dout._gpio.port, hx_ptr->base_dout._gpio.pin));
+}
+
+// - - - -
+
+static void SOOL_HX711_SetHelperPinTIM(FunctionalState state) {
+
+	if ( state == ENABLE ) {
+		SOOL_Periph_GPIO_SetBits(GPIOA, GPIO_Pin_12);
+	} else {
+		SOOL_Periph_GPIO_ResetBits(GPIOA, GPIO_Pin_12);
+	}
+
+}
+
+static void SOOL_HX711_SetHelperPinEXTI(FunctionalState state) {
+
+	if ( state == ENABLE ) {
+		SOOL_Periph_GPIO_SetBits(GPIOA, GPIO_Pin_9);
+	} else {
+		SOOL_Periph_GPIO_ResetBits(GPIOA, GPIO_Pin_9);
+	}
+
+}
