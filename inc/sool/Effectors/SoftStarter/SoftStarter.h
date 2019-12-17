65f7a42ba5afe826fa6e59738fb3318b01a0caa7
Jaroslaw Karwowski <j.karwowski@stud.elka.pw.edu.pl>
2019-12-17 16:19:46 +0100

    Effectors/SoftStarted - new class added, works OK
    tested

---
+++ inc/sool/Effectors/SoftStarter/SoftStarter.h
@@ -0,0 +1,88 @@
+/*
+ * SoftStarter.h
+ *
+ *  Created on: 17.12.2019
+ *      Author: user
+ */
+
+#ifndef INC_SOOL_EFFECTORS_SOFTSTARTER_SOFTSTARTER_H_
+#define INC_SOOL_EFFECTORS_SOFTSTARTER_SOFTSTARTER_H_
+
+#include <stdint.h>
+
+struct _SOOL_SoftStarterStruct;
+typedef struct _SOOL_SoftStarterStruct SOOL_SoftStarter;
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+struct _SOOL_SoftStarterConfigStruct {
+	uint16_t pulse_start;
+	uint16_t increments;
+};
+
+struct _SOOL_SoftStarterSetupStruct {
+	int16_t  pulse_change;
+	uint32_t time_change_gap;
+};
+
+struct _SOOL_SoftStarterStateStruct {
+	uint16_t pulse_last;
+	uint16_t changes_left; // how many times the pulse will be incremented/decremented
+	uint32_t time_last_pulse_change;
+};
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+struct _SOOL_SoftStarterStruct {
+
+	struct _SOOL_SoftStarterSetupStruct	 _setup;
+	struct _SOOL_SoftStarterStateStruct  _state;
+	struct _SOOL_SoftStarterConfigStruct _config;
+
+	// DEPRECATED, call the constructor once again instead
+//	void (*Reconfigure)(SOOL_SoftStarter* ss_ptr, uint16_t pulse_min, uint16_t pulse_max, uint32_t duration);
+
+	/**
+	 * @brief Starts the soft starting procedure. Saves the timestamp (milliseconds) internally.
+	 * @param ss_ptr
+	 */
+	void (*Start)(SOOL_SoftStarter* ss_ptr);
+
+	/**
+	 * @brief Processes the possible change of the pulse value (according to the timestamp)
+	 * @param ss_ptr
+	 * @return Returns the previous, valid `pulse` value or the newly calculated one
+	 */
+	uint16_t (*Process)(SOOL_SoftStarter* ss_ptr);
+
+	/**
+	 * @brief Evaluates whether the soft start procedure has finished
+	 * @param ss_ptr
+	 * @return
+	 * @note The internal state flag `finished` is not cleared after this call (stays until the next `Start` call).
+	 */
+	uint8_t (*IsFinished)(const SOOL_SoftStarter* ss_ptr);
+
+};
+
+// --------------------------------------------------------------
+
+/**
+ * @brief Provides linear change of the pulse value (timer-specific) along the time given
+ * by `duration` parameter. The ramp can be of a rising or falling tendency (adjustable
+ * via pulse_start > pulse_end or pulse_start < pulse_end).
+ * @param pulse_start
+ * @param pulse_end
+ * @param duration: how long the soft starting procedure should take [in milliseconds]
+ * @return
+ * @note This class requires SysTick timer to be configured by default, call
+ * 		 @ref SOOL_Periph_TIM_SysTick_DefaultConfig at the startup of the MCU.
+ * @note If there is an instance in the application which blocks the MCU operation
+ * for more than 1 millisecond then SoftStart procedure will not work properly
+ * (time resolution of the `pulse` increment is 1 ms).
+ */
+extern SOOL_SoftStarter SOOL_Effector_SoftStarter_Initialize(uint16_t pulse_start, uint16_t pulse_end, uint32_t duration);
+
+// --------------------------------------------------------------
+
+#endif /* INC_SOOL_EFFECTORS_SOFTSTARTER_SOFTSTARTER_H_ */
---
+++ src/sool/Effectors/SoftStarter/SoftStarter.c
@@ -0,0 +1,134 @@
+/*
+ * SoftStarter.c
+ *
+ *  Created on: 17.12.2019
+ *      Author: user
+ */
+
+#include <sool/Effectors/SoftStarter/SoftStarter.h>
+#include <sool/Peripherals/TIM/SystickTimer.h>
+
+static void SoftStarter_Start(SOOL_SoftStarter* ss_ptr);
+static uint16_t SoftStarter_Process(SOOL_SoftStarter* ss_ptr);
+static uint8_t SoftStarter_IsFinished(const SOOL_SoftStarter* ss_ptr);
+
+// --------------------------------------------------------------
+
+SOOL_SoftStarter SOOL_Effector_SoftStarter_Initialize(uint16_t pulse_start, uint16_t pulse_end, uint32_t duration) {
+
+	/* Newly created instance */
+	SOOL_SoftStarter ss;
+
+	/* Evaluate correctness */
+	if ( pulse_end < 0 || pulse_start < 0 ) {
+		/* ERROR */
+		return (ss);
+	} else if ( pulse_end == pulse_start ) {
+		/* Does not make sense */
+		return (ss);
+	}
+
+	/* Fill the state structure */
+	ss._state.pulse_last = pulse_start;
+	ss._state.changes_left = 0; // temporary
+	ss._state.time_last_pulse_change = 0; // does not matter at that moment
+
+	/* Fill the setup structure */
+	ss._setup.pulse_change = +1;
+
+	/* Check which pulse value is bigger to properly calculate the change of the pulse */
+	uint16_t pulse_bigger, pulse_smaller = 0;
+	if ( pulse_end > pulse_start ) {
+		pulse_bigger = pulse_end;
+		pulse_smaller = pulse_start;
+	} else {
+		pulse_bigger = pulse_start;
+		pulse_smaller = pulse_end;
+	}
+
+	/* Check feasibility of the soft-starting procedure (with the hard-coded time resolution) */
+	if ( (pulse_bigger - pulse_smaller) > duration ) {
+
+		/* Cannot make changes as fast as required, but can still process the soft-start
+		 * as fast as possible. The `pulse_change` will be adjusted properly */
+		ss._setup.time_change_gap = 1;
+
+		/* Calculate pulse change */
+		ss._setup.pulse_change = (pulse_bigger - pulse_smaller) / duration; // must be > 1
+
+		/* Set the number of increments/decrements */
+		ss._state.changes_left = duration;
+
+	} else {
+
+		/* (pulse_bigger - pulse_smaller) <= duration */
+
+		/* Normal operation - single increment of the `pulse` in each event. */
+		ss._setup.time_change_gap = duration / (pulse_bigger - pulse_smaller);
+
+		/* Pulse change -> +1 or -1 */
+
+		/* Set the number of increments/decrements */
+		ss._state.changes_left = pulse_bigger - pulse_smaller;
+
+	}
+
+	/* Adjust the sign of the `pulse change` - if needed */
+	if ( pulse_end < pulse_start ) {
+		ss._setup.pulse_change = -ss._setup.pulse_change;
+	}
+
+	/* Fill the config structure */
+	ss._config.increments = ss._state.changes_left;
+	ss._config.pulse_start = pulse_start;
+
+	/* Method pointers */
+	ss.IsFinished = SoftStarter_IsFinished;
+	ss.Process = SoftStarter_Process;
+	ss.Start = SoftStarter_Start;
+
+	return (ss);
+
+}
+
+// --------------------------------------------------------------
+
+static void SoftStarter_Start(SOOL_SoftStarter* ss_ptr) {
+	ss_ptr->_state.time_last_pulse_change = SOOL_Periph_TIM_SysTick_GetMillis();
+	ss_ptr->_state.changes_left = ss_ptr->_config.increments;
+	ss_ptr->_state.pulse_last = ss_ptr->_config.pulse_start;
+}
+
+// --------------------------------------------------------------
+
+static uint16_t SoftStarter_Process(SOOL_SoftStarter* ss_ptr) {
+
+	if ( (SOOL_Periph_TIM_SysTick_GetMillis() - ss_ptr->_state.time_last_pulse_change) >= ss_ptr->_setup.time_change_gap ) {
+
+		/* Check if finished */
+		if ( ss_ptr->_state.changes_left == 0 ) {
+			return (ss_ptr->_state.pulse_last);
+		}
+
+		/* Update timestamp */
+		ss_ptr->_state.time_last_pulse_change = SOOL_Periph_TIM_SysTick_GetMillis();
+		ss_ptr->_state.changes_left--;
+		ss_ptr->_state.pulse_last += ss_ptr->_setup.pulse_change;
+
+	}
+	return (ss_ptr->_state.pulse_last);
+
+}
+
+// --------------------------------------------------------------
+
+static uint8_t SoftStarter_IsFinished(const SOOL_SoftStarter* ss_ptr) {
+
+	if ( ss_ptr->_state.changes_left == 0 ) {
+		return (1);
+	}
+	return (0);
+
+}
+
+// --------------------------------------------------------------
