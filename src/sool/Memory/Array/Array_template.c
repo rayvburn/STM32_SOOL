6d1ff8b5874de219282990b2382da646c10dcb6c
Jaroslaw Karwowski <j.karwowski@stud.elka.pw.edu.pl>
2020-03-17 00:27:30 +0100

    only the ignored files left

--- src/sool/Memory/Array/Array_template.c
+++
@@ -1,8 +0,0 @@
-/*
- * Array_template.c
- *
- *  Created on: 14.05.2019
- *      Author: user
- */
-
-
--- src/sool/Sensors/HallSensor/HallAnalog.c
+++
@@ -1,8 +0,0 @@
-/*
- * HallAnalog.c
- *
- *  Created on: 24.06.2019
- *      Author: user
- */
-
-#include "sool/Sensors/HallSensor/HallAnalog.h"
